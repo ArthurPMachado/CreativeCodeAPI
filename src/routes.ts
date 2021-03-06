8e487c4737d880bb6f0c665b799cfa5aae82eba8
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-06 19:25:48 -0300

    Feat: add routes for users and address

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+const router = Router();
+/**
+ router.get('/users');
+ router.get('/users');
+ router.post('/users');
+ router.put('/users');
+ router.delete('/users');
+
+ router.get('/address');
+ router.get('/address');
+ router.post('/address');
+ router.put('/address');
+ router.delete('/address');
+
+*/
+
+export default router;
