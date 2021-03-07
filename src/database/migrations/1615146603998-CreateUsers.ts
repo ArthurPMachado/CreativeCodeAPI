fa5cc7368f4086e3184c5a3f0540fe9799b09f6c
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-07 17:04:30 -0300

    Feat: add user migration

---
+++ src/database/migrations/1615146603998-CreateUsers.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1615146603998 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'nome',
+            type: 'varchar',
+          },
+          {
+            name: 'telefone',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'idade',
+            type: 'integer',
+          },
+          {
+            name: 'peso',
+            type: 'decimal',
+          },
+          {
+            name: 'peso',
+            type: 'varchar',
+            enum: ['BRANCO', 'NEGRO', 'PARDO', 'INDIGENA', 'ASIATICO'],
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
