66862f189b78dc548dd131df9f10b34e8fab2096
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-07 17:18:51 -0300

    Feat: add create Address migration

---
+++ src/database/migrations/1615147542761-CreateAddress.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateAddress1615147542761 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'address',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'nome_endereco',
+            type: 'varchar',
+          },
+          {
+            name: 'numero',
+            type: 'integer',
+          },
+          {
+            name: 'complemento',
+            type: 'varchar',
+          },
+          {
+            name: 'cep',
+            type: 'integer',
+          },
+          {
+            name: 'cidade',
+            type: 'varchar',
+          },
+          {
+            name: 'estado',
+            type: 'varchar',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKUser',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('address');
+  }
+}
