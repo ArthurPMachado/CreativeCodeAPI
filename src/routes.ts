00cb81908a4238f6aba842e27839a9b222fc0650
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-07 20:51:35 -0300

    Feat: add post route to address

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+import AddressController from './controllers/AddressController';
 
 import UserController from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
+const addressController = new AddressController();
 
 router.post('/users', userController.create);
+router.post('/address', addressController.create);
 /**
  router.get('/users', userController.create);
  router.get('/users');
@@ -15,7 +18,6 @@ router.post('/users', userController.create);
 
  router.get('/address');
  router.get('/address');
- router.post('/address');
  router.put('/address');
  router.delete('/address');
 
