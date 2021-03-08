8c3a3b7a71def7c95601e4c1f7eeb33a98826460
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-08 15:11:53 -0300

    Feat: add auth service

---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-return-assign */
+const { verify } = require('../services/token');
+
+const extractToken = (ctx) => {
+  const authorization = ctx.headers.authorization || '';
+  return authorization.replace('Bearer ', '');
+};
+
+const handleError = (error) => {
+  console.error('Failed to verify token', error);
+
+  /* eslint-disable prefer-promise-reject-errors */
+  return Promise.reject({
+    status: 401,
+    message: 'Invalid authentication token',
+    code: 'UNAUTHENTICATED',
+  });
+};
+
+module.exports = (ctx, next) => {
+  const token = extractToken(ctx);
+  return verify(token)
+    .catch(handleError)
+    .then(({ role }) => (ctx.state.role = role))
+    .then(next);
+};
